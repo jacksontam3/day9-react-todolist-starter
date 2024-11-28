f0d9b506e0020cc8fffef4f673ba60bd3c0e125a
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-28 22:55:29 +0800

    feat: implement todo item with remove and toggle using dispatch

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,22 @@
-const TodoItem = (prop) => {
+import { TodoListContext } from "../context/TodoListContext";
+import { useContext } from "react";
+
+const TodoItem = ({todo}) => {
+    const { dispatch } = useContext(TodoListContext);
+
+    const handleToggleTodo = () => {
+        dispatch({ type: "TOGGLE", payload: todo.id });
+    };
+
+    const removeTodo = () => {
+        dispatch({ type: "REMOVE", payload: todo.id })
+    };
     return (
-        <div>
-            <div>{prop.todo.text}</div>
-            <button>X</button>
+        <div className={"todo-item-wrapper"}>
+            <div className={`todo-text ${todo.done ? "done" : ""}`} onClick={handleToggleTodo}>{todo.text}</div>
+            <button onClick={removeTodo}>X</button>
         </div>
     );
 }
 
-export default TodoItem
+export default TodoItem;
