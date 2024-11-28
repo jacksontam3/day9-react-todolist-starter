0f5aa03f4513d74d6ce2ef12aa0ddd03a39d1908
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-28 21:50:08 +0800

    feat: create remove case for Reducer

--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -7,6 +7,8 @@ export const todoReducer = (state, action) => {
   switch(action.type){
     case 'ADD':
       return [...state, {id: Date.now(), text: action.payload, done: false}]
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
