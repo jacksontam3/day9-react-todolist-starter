def5b8389242972072e2d6590c5753f3421130c9
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-29 10:12:27 +0800

    feat: create route to todolist

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
 import {TodoListProvider} from "./context/TodoListContext";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <TodoListProvider>
-        <TodoList/>
+          <Router>
+              <Routes>
+                  <Route path={"/todo-list"}  element = {<TodoList/>}>
+                  </Route>
+              </Routes>
+          </Router>
       </TodoListProvider>
     </div>
   );
