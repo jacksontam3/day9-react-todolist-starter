7761a437457a4e1e202d06c63363ec126b64d9cc
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-30 22:02:57 +0800

    feat: create help page using route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoList from "./components/TodoList";
 import {TodoListProvider} from "./context/TodoListContext";
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import DoneList from "./components/DoneList";
+import HelpPage from "./components/HelpPage";
 
 function NotFoundPage (){
     return <div>NotFoundPage</div>
@@ -15,13 +16,15 @@ function App() {
       <TodoListProvider>
           <Router>
               <nav>
-                  <Link to={"/"}>Home</Link> | <Link to={"/done-list"}>Done-List</Link>
+                  <Link to={"/"}>Home</Link> | <Link to={"/done-list"}>Done-List</Link> | <Link to={"/todo-list"}>Todo-List</Link> | <Link to={"/helpPage"}>Help</Link>
               </nav>
               <Routes>
                   <Route path = {"/"} element={<TodoList/>}></Route>
                   <Route path={"/done-list"} element={<DoneList/>}></Route>
                   <Route path={"/todo-list"}  element = {<TodoList/>}></Route>
+                  <Route path={"/helpPage"} element = {<HelpPage/>}></Route>
                   <Route path={"*"} element = {<NotFoundPage/>}></Route>
+
               </Routes>
           </Router>
       </TodoListProvider>
---
+++ src/components/HelpPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelpPage = () => {
+    return (
+        <div>
+            <h1>This is the Help Page</h1>
+            <p>If you need assistance, please contact me.</p>
+        </div>
+    );
+};
+
+export default HelpPage;
