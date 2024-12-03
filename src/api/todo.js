893ee04a406bba5244c314940e90abc2dcbeabc6
Jackson Tam <Jackson.Tam@OOCL.com>
2024-12-03 08:58:04 +0800

    fix: edit todo item to catch result of backend data

--- src/api/todo.js
+++ src/api/todo.js
@@ -2,23 +2,23 @@ import { instance } from "./interceptor";
 
 export const getTodoList = async () => {
     const response = await instance.get("TodoItem");
-    return response.data;
+    return response.data.result;
 }
 
 export const getTodoById = async (id) => {
     const response = await instance.get(`TodoItem/${id}`);
-    return response.data;
+    return response.data.result;
 }
 
 export const addTodoList = async (newData) => {
     const response = await instance.post("TodoItem", newData);
-    return response.data;
+    return response.data.result;
 }
 
 export const deleteById = async (id) => {
     try {
         const response = await instance.delete(`TodoItem/${id}`);
-        return response.data;
+        return response.data.result;
     } catch (error) {
         throw error;
     }
@@ -27,7 +27,7 @@ export const deleteById = async (id) => {
 export const update = async (newData) => {
         try {
             const response = await instance.put(`TodoItem/${newData.id}`, newData);
-            return response.data;
+            return response.data.result;
         } catch (error) {
             throw error;
         }
